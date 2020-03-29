6d687a786ca436225009d22b36bb4ef2c8aedbf5
Avnish <avnish.desai@gmail.com>
2020-03-29 12:27:57 +0200

    Allow CORS
    Signed-off-by: Avnish <avnish.desai@gmail.com>

--- src/main/kotlin/org/baps/krl/Controllers/QuestionsController.kt
+++ src/main/kotlin/org/baps/krl/Controllers/QuestionsController.kt
@@ -3,8 +3,8 @@ package org.baps.krl.Controllers
 import org.baps.krl.Services.QuestionsService
 import org.baps.krl.Services.RoundsService
 import org.baps.krl.db.Question
-import org.baps.krl.db.Round
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,11 +15,13 @@ class QuestionsController(
 ) {
 
     @GetMapping("/")
+    @CrossOrigin()
     fun getQuestions(): Iterable<Question>{
         return questionsService.getQuestions()
     }
 
     @GetMapping("/getCurrentQuestions")
+    @CrossOrigin()
     fun getCurrentQuestions(): Iterable<Question>{
 
         return questionsService.getQuestionsByRound(roundsService.getCurrentRound())
